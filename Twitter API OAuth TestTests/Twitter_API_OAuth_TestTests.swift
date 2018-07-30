e945831ec900f0f4e010c53c317129b916afcc8f
Robert Hunter <rlhunter@roberts-macbook-pro.local>
2018-07-30 09:00:07 -0400

    Initial Commit

---
+++ Twitter API OAuth TestTests/Twitter_API_OAuth_TestTests.swift
@@ -0,0 +1,36 @@
+//
+//  Twitter_API_OAuth_TestTests.swift
+//  Twitter API OAuth TestTests
+//
+//  Created by Robert Hunter on 7/30/18.
+//  Copyright © 2018 Lance Hunter. All rights reserved.
+//
+
+import XCTest
+@testable import Twitter_API_OAuth_Test
+
+class Twitter_API_OAuth_TestTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
